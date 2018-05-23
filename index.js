8c5a7e4c55aefb6a0692af8838ea30f53ac3a96e
igunwiguna <igunwiguna@gmail.com>
2018-05-23 22:43:11 +0700

    Instalasi pertama React-Native

---
+++ index.js
@@ -0,0 +1,9 @@
+import { AppRegistry } from 'react-native';
+import App from './app/App';
+
+AppRegistry.registerComponent('BusDamri', () => App);
+
+// import  { AppRegistry } from 'react-native' ;
+// import app from './app';
+
+// AppRegistry.registerComponent('BusDamri', ()=> app);
